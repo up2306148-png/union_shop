e4c79ce6d87748e821023c4a1010a1753d3b9e4e
up2306148-png <metlapranay6@gmail.com>
2025-12-04 13:49:15 +0000

    working before footer on all pages

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class UnionShopApp extends StatelessWidget {
       // When navigating to '/product', build and return the ProductPage
       // In your browser, try this link: http://localhost:49856/#/product
       routes: {
-        '/product': (context) => const ProductPage(),
+        '/product': (context) => ProductPage(),
         '/about': (context) => const AboutUsScreen(),
         '/signin': (context) => const SignInScreen(),
         '/collections': (context) => const CollectionsScreen(),
